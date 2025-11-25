62f03bc49aa0f1493c9f576312b5e9789fe5ac9f
DUY2602 <duydo7779@gmail.com>
2025-11-25 09:32:37 +0700

    test.yml

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,14 +2,13 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase; // 🚨 Thêm dòng này
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+    use RefreshDatabase; // 🚨 Thêm trait này
+
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
